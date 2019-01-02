315fd86785b7d2d481c89bee3b066c57cd010620
windcoder <zhrucong@gmail.com>
2019-01-03 01:49:01 +0800

    -m追加站位

---
+++ config.js
@@ -0,0 +1,21 @@
+// var host = 'https://book.windcoder.com/nightbook';
+// var host = 'http://localhost:8080';
+var host = 'http://localhost:8000';
+var config = {
+  pid:1,
+  // 下面的地址配合云端 Demo 工作
+  service: {
+    host,
+
+    // 登录地址，用于建立会话
+    // loginUrl: `https://${host}/login`,
+    loginUrl: `${host}/auth/home`,
+    apiUrlhead: `${host}/jsapi/`,
+    // 测试的请求地址，用于测试会话
+    requestUrl: `https://${host}/user`,
+
+    // 测试的信道服务地址
+    tunnelUrl: `https://${host}/tunnel`,
+  }
+};
+module.exports = config;
---
+++ pages/index/index.js
@@ -0,0 +1,151 @@
+// pages/userInfo/userInfo.js
+//获取应用实例
+const app = getApp()
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    tabs: ["在读", "未读", "已读", "所有"],
+    activeIndex: 0,
+    sliderOffset: 0,
+    sliderOffsetUnit:0,
+    sliderLeft: 0,
+    readType:"在读",
+    readNum:0,
+    grids: [0, 1, 2, 3, 4, 5, 6, 7, 8],
+    imgMode: 'aspectFit',
+    imgWith:300,
+    imgHeigh:400,
+    imgSrc:'../../image/test_book.jpg',
+    scrollTop: 0,
+    scrollHeight:0,
+
+    sessionStr:null,
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+
+    showModalStatus: false,
+    showNavStatus:false,
+    readStatuss: ["在读", "未读", "已读"],
+    readStatusIndex: 0,
+    nowBookIbj:{
+
+    },
+    addBookObj:{
+      info:null,
+      oldReadStatus:1,
+      newReadStatus:1,
+      addType:1,//2 之前已存在 1新添加
+    },
+    readObjs:{
+      pageSize:10
+    },
+    readNowObj:{
+      pageInfo:{
+        pageSize:0,
+        currentPage:1,
+        totalResultSize:0
+      },
+      bottomType: false,
+      isAlllond: false,
+      readObjs:[],
+    },
+    readNoObj:{
+      pageInfo: {
+        pageSize: 0,
+        currentPage: 1,
+        totalResultSize: 0
+      },
+      bottomType:false,
+      isAlllond: false,
+      readObjs: [],
+    },
+    readFinishObj:{
+      pageInfo: {
+        pageSize: 0,
+        currentPage: 1,
+        totalResultSize: 0
+      },
+      bottomType: false,
+      isAlllond:false,
+      readObjs: [],
+    },
+    readAllObj:{
+      pageInfo: {
+        pageSize: 0,
+        currentPage: 1,
+        totalResultSize: 0
+      },
+      bottomType: false,
+      isAlllond: false,
+      readObjs: [],
+    }
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+  getUserInfo: function(e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  }
+})
